92e75834fb7b375e10ed707b25457d2f628939da
Pratyay Ganguli <pratyay.ganguli@widas.in>
2026-04-01 10:42:15 +0530

    fix([1856](cidaas-v2/framework/issues#1856)): fix lowercase issue for reading and deleting scopes

--- helpers/cidaas/scope.go
+++ helpers/cidaas/scope.go
@@ -68,7 +68,7 @@ func (c *Scope) Upsert(ctx context.Context, sc ScopeModel) (*ScopeResponse, erro
 
 func (c *Scope) Get(ctx context.Context, scopeKey string) (*ScopeResponse, error) {
 	var response ScopeResponse
-	url := fmt.Sprintf("%s/%s?scopekey=%s", c.BaseURL, "scopes-srv/scope", strings.ToLower(scopeKey))
+	url := fmt.Sprintf("%s/%s?scopekey=%s", c.BaseURL, "scopes-srv/scope", scopeKey)
 	client, err := util.NewHTTPClient(url, http.MethodGet, c.AccessToken)
 	if err != nil {
 		return nil, err
@@ -86,7 +86,7 @@ func (c *Scope) Get(ctx context.Context, scopeKey string) (*ScopeResponse, error
 }
 
 func (c *Scope) Delete(ctx context.Context, scopeKey string) error {
-	url := fmt.Sprintf("%s/%s/%s", c.BaseURL, "scopes-srv/scope", strings.ToLower(scopeKey))
+	url := fmt.Sprintf("%s/%s/%s", c.BaseURL, "scopes-srv/scope", scopeKey)
 	client, err := util.NewHTTPClient(url, http.MethodDelete, c.AccessToken)
 	if err != nil {
 		return err
